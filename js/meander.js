be69d579adbf72f3f6851223097f7573211f9fb6
Simon Roy <simon.roy1211@gmail.com>
2024-02-28 22:12:30 -0500

    Added some change and quick fixed range thumb

--- js/meander.js
+++ js/meander.js
@@ -1,7 +1,7 @@
 const MEANDER_DEFAULT_SPEED = 100;
 const MEANDER_SEGMENT_COUNT = 20;
 
-const DEFAULT_WIDTH_RANGE  = { "min": "0", "max": "50", "value": "5", "step": "1" };
+const DEFAULT_WIDTH_RANGE  = { "min": "0", "max": "500", "value": "5", "step": "1" };
 const DEFAULT_SPEED_RANGE  = { "min": "0", "max": "1000", "value": "50", "step": "1" };
 const DEFAULT_LENGTH_RANGE = { "min": "4", "max": "50" };
 
