3df196c6724100782ff0adc03c524bfa45109c18
penguinboi <simon.roy1211@gmail.com>
2024-03-19 02:51:45 -0400

    Converted meander to an object

--- js/meander.js
+++ js/meander.js
@@ -5,33 +5,7 @@ const DEFAULT_WIDTH_RANGE  = { "min": "0", "max": "400", "value": "50", "step": 
 const DEFAULT_SPEED_RANGE  = { "min": "0", "max": "1000", "value": "50", "step": "1" };
 const DEFAULT_LENGTH_RANGE = { "min": "4", "max": "50", "value": "20" };
 
-
-let meander;
-
-let speed = MEANDER_DEFAULT_SPEED;
-let segmentCount = MEANDER_SEGMENT_COUNT;
-let curvatures = [];
-
-let animated = true
-
-function range(name, options) {
-    let range = document.querySelector(`[name='${name}']`);
-
-    range.min = options["min"];
-    range.max = options["max"];
-    range.step = options["step"];
-    range.value = options["value"];
-
-    return range;
-}
-
-function generateRiver(speed, width, height) {
-    const path = new Path({
-        strokeColor: '#000000',
-        strokeWidth: DEFAULT_WIDTH_RANGE["value"],
-        strokeCap: 'round'
-    });
-
+function simpleRiverAlgorithm(path, speed, width, height, segmentCount, curvatures) {
     let startPoint = new Point(width / 2, 0);
     let segmentLength = height / segmentCount;
     
@@ -54,24 +28,92 @@ function generateRiver(speed, width, height) {
     }
 
     path.smooth();
-
-    if (meander) {
-        path.strokeWidth = meander.strokeWidth;
-        meander.remove();
-    }
-
-    meander = path;
+    return path;
 }
 
-window.onload = function () {
+class Meander {
+    constructor(width, height) {
+        this.animated = true;
+
+        this._speed = MEANDER_DEFAULT_SPEED;
+        this._width = width;
+        this._height = height;
+
+        this.path = null;
+        this.segmentCount = MEANDER_SEGMENT_COUNT;
+        this.curvatures = []
+
+        this.algorithm = simpleRiverAlgorithm
+    }
+
+    generate() {
+        const path = new Path({
+            strokeColor: '#000000',
+            strokeWidth: DEFAULT_WIDTH_RANGE["value"],
+            strokeCap: 'round'
+        });
+
+        if (this.path) {
+            path.strokeWidth = this.path.strokeWidth;
+            this.path.remove();
+        }
+
+        this.curvatures = [];
+        this.path = this.algorithm(
+            path,
+            this._speed,
+            this._width, 
+            this._height, 
+            this.segmentCount,
+            this.curvatures
+        );
+    }
+
+    setSpeed(value) {
+        this.speed = value;
+        this.generate();
+    }
+
+    setWidth(value) {
+        this.path.strokeWidth = Math.max(value, 1);
+    }
+
+    setLength(value) {
+        this.segmentCount = value;
+        this.generate();
+    }
+
+    toggleAnimation() {
+        this.animated = !this.animated;
+    }
+}
+
+window.onload = function() {
     paper.install(window);
     paper.setup("meanderCanvas");
 
-    generateRiver(speed, view.size.width, view.size.height);
+    const meander = new Meander(view.size.width, view.size.height);
+    meander.generate();
+
+    function range(name, options) {
+        let range = document.querySelector(`[name='${name}']`);
+
+        range.min = options["min"];
+        range.max = options["max"];
+        range.step = options["step"];
+        range.value = options["value"];
+
+        return range;
+    }
+
+    function radio(name, func) {
+        document.querySelectorAll(`[name='${name}']`).forEach(element => {
+            element.onchange = (e) => func(e.target)
+        });
+    }
 
     document.getElementById("generate").onclick = function() {
-        curvatures = [];
-        generateRiver(speed, view.size.width, view.size.height);
+        meander.generate();
     }
 
     document.getElementById("exportSVG").onclick = function() {
@@ -79,7 +121,7 @@ window.onload = function () {
         let url = "data:image/svg+xml;utf8," + encodeURIComponent(svg);
         let link = document.createElement("a");
 
-        link.download = "meander.svg";
+        link.download = "meanderPath.svg";
         link.href = url;
         
         link.click();
@@ -87,32 +129,38 @@ window.onload = function () {
     }
 
     document.getElementById("toggleAnimation").onclick = function() {
-        animated = !animated;
+        meander.toggleAnimation()
 
-        // I feel like stuff like that could be done elsewhere
         let img = this.getElementsByTagName("img")[0];
-        img.src = animated ? "images/play.svg" : "images/pause.svg"
+        img.src = meander.animated ? "images/play.svg" : "images/pause.svg"
     }
 
     range("width", DEFAULT_WIDTH_RANGE).oninput = function() {
-        meander.strokeWidth = Math.max(this.value, 1);
+        meander.setWidth(this.value);
     }
 
     range("length", DEFAULT_LENGTH_RANGE).oninput = function() {
-        segmentCount = parseInt(this.value);
-        generateRiver(speed, view.size.width, view.size.height);
+        meander.setLength(parseInt(this.value));
     }
 
     range("speed", DEFAULT_SPEED_RANGE).oninput = function() {
-        speed = parseInt(this.value);
-        generateRiver(speed, view.size.width, view.size.height);
+        meander.setSpeed(parseInt(this.value));
     };
 
-    view.onFrame = function(event) {
-        if (!animated || speed == 0) return;
+    radio("trace", (e) => {
+        console.log(e.value);
+        meander.path.strokeColor = 0;
+    });
 
-        for (let i = 1; i < segmentCount - 1; i++) {
-            let segment = meander.segments[i];
+    radio("algorithm", (e) => {
+        console.log(e.value);
+    });
+
+    view.onFrame = function(event) {
+        if (!meander.animated || meander.speed == 0) return;
+
+        for (let i = 1; i < meander.segmentCount - 1; i++) {
+            let segment = meander.path.segments[i];
 
             if (segment) {
                 let sinus = Math.sin(event.time + i);
@@ -125,6 +173,6 @@ window.onload = function () {
             }
         }
 
-        meander.smooth();
+        meander.path.smooth();
     }
 }
